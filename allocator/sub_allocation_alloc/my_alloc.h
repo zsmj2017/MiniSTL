dd8950e7697f5235a7a7f4bdbb941f2ca901396c
zsmj2017 <sphinxs@vip.qq.com>
2018-07-22 15:18:23 +0800

    Add unintialized.h,add memory pool for alloc.h

--- allocator/sub_allocation_alloc/my_alloc.h
+++ allocator/sub_allocation_alloc/my_alloc.h
@@ -79,6 +79,7 @@ typedef __malloc_alloc_template<0> malloc_alloc;
 //freelist参数设定
 //区块上调边界，区块上限，freelist个数
 //此处使用C++11 scoped enum反而由于强类型而导致下文将处处使用static_cast，因此不用
+//Effective C++所述enum惯用法
 enum  __freelist_setting:std::size_t
 {__ALIGN=8, __MAX_BYTES = 128, __NFREELISTS = __MAX_BYTES / __ALIGN};
 
@@ -97,8 +98,8 @@ private:
 	//free_list节点
 	//由于union特性，并不需要占用额外的内存
 	union obj {
-		union obj* free_list_link;
-		char client_data[1];
+		union obj* free_list_link;//指向下一节点
+		char client_data[1];//指向资源
 	};
 private:
 	static obj* volatile free_list[__NFREELISTS];
@@ -110,7 +111,7 @@ private:
 	static void* refill(size_t n);
 	//配置一大块空间，可容纳nobjs个大小为size的区块
 	//如果不便配置 nobjs可能会降低
-	static char* chunk_alloc(size_t n, int &objs);
+	static char* chunk_alloc(size_t size, int &nobjs);
 
 	//chunk allocation state
 	static char *start_free;//内存池起始位置，只在chunk_alloc()中变化
@@ -124,14 +125,103 @@ public:
 
 //以下为static data member的定义与初值设定
 template <bool threads, int inst>
-char* __default_alloc_template<threads, inst>::start_free = 0;
+char* __default_alloc_template<threads, inst>::start_free = nullptr;
 
 template <bool threads, int inst>
-char* __default_alloc_template<threads, inst>::end_free = 0;
+char* __default_alloc_template<threads, inst>::end_free = nullptr;
 
 template <bool threads, int inst>
 __default_alloc_template<threads, inst>::obj* volatile
 __default_alloc_template<threads, inst>::free_list[__NFREELISTS] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+
+//当free_list无可用区块时，重新填充空间
+//新空间取自内存池，默认获取20个节点(区块）
+//若内存池不足，则获取的将小于20
+template<bool threads, int inst>
+inline void* __default_alloc_template<threads, inst>::refill(size_t n){
+	int nobjs = 20;
+	//尝试调用chunk_alloc,注意nobjs以pass-by-reference传入
+	char* chunk = chunk_alloc(n, nobjs);
+	obj* volatile *my_free_list;
+	obj* result;
+	obj* current_obj, next_obj;
+
+	//若只获取了一个区块则直接分配给调用者，不加入free_list
+	if (1 == nobjs) return(chunk);
+	my_free_list = free_list + FREELIST_INDEX(n);
+
+	//在chunk空间内建立free_list
+	result = static_cast<obj*>(chunk);
+	//引导free_list指向内存池分配的空间
+	//chunk指向的内存直接分给用户，free_list指向剩下（19或更少）的区块
+	*my_free_list = next_obj = static_cast<obj*>(chunk + n);
+	for (int i = 1;i!=nobjs-1; ++i) {
+		current_obj = next_obj;
+		//先强转为char*是因为chunk为char*
+		next_obj = static_cast<obj*>(static_cast<char*>(next_obj) + n);
+	    current_obj->free_list_link = next_obj;
+	}
+	next_obj->free_list_link = nullptr;
+	return result;
+}
+
+//默认size为8的整数倍
+template<bool threads, int inst>
+inline char* __default_alloc_template<threads, inst>::chunk_alloc(size_t size, int & nobjs){
+	char* result;
+	size_t total_bytes = size * nobjs;
+	size_t bytes_left = end_free - start_free;//内存池剩余空间
+	if (bytes_left > total_bytes) {
+		//容量满足需求
+		result = start_free;
+		start_free += total_bytes;
+		return result;
+	}
+	else if (bytes_left > size) {
+		//容量至少满足一个区块需求
+		nobjs = bytes_left / size;
+		total_bytes = size * nobjs;
+		result = start_free;
+		start_free += total_bytes;
+		return result;
+	}
+	else {
+		//内存池一个区块都无法提供
+		size_t bytes_to_get = 2 * total_bytes + ROUND_UP(heap_size >> 4);//向heap申请诸如的内存，heap_size将随着配置次数增加而增加
+		if (bytes_left > 0) {
+			//当前内存池还有一部分内存，为了不浪费分配给free_list
+			obj* volatile *my_free_list = free_list + FREELIST_INDEX(bytes_left);
+			static_cast<obj*>(start_free)->free_list_link = my_free_list;
+			*my_free_list = static_cast<obj*>(start_free);
+		}
+
+		//配置heap空间以补充内存池
+		start_free = static_cast<char*>(malloc(bytes_to_get));
+		if (!start_free) {
+			//heap空间不足，malloc失败
+			obj* volatile *my_free_list, *p;
+			//在free_list中检查是否有符合需求的区块
+			for (i = size; i <= __MAX_BYTES; i += __ALIGN) {
+				my_free_list = free_list + FREELIST_INDEX(i);
+				p = *my_free_list;
+				if (!p) {
+					//存在足以分配的区块
+					*my_free_list = p->free_list_link;//抽离当前区块
+					start_free = static_cast<char*>(p);
+					end_free = start_free + i;
+					return(chunk_alloc(size, nobjs));//递归调用以修正nobjs，此时必然进入else_if分支
+				}
+			}
+			end_free = nullptr;//到处都找不到内存
+			//调用第一级适配器观察其能否分配内存，或抛出异常
+			start_free = static_cast<char*>(malloc_alloc::allocate(bytes_to_get));
+		}
+		heap_size += bytes_to_get;//已占用的堆内存
+		end_free += start_free + bytes_to_get;
+		return chunk_alloc(size, nobjs);//调用自身以修正nobjs
+	}
+}
 
 template<bool threads, int inst>
 inline void* __default_alloc_template<threads, inst>::allocate(size_t n){
--- allocator/sub_allocation_alloc/my_uninitialized.h
+++ allocator/sub_allocation_alloc/my_uninitialized.h
@@ -1,1 +1,95 @@
 #pragma once
+#include"my_construct.h"
+
+template <class InputIterator, class ForwardIterator>
+inline ForwardIterator uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result) {
+	return __uninitialized_copy(first, last, result, value_type(first));
+}
+
+template<class InputIterator, class ForwardIterator, class T>
+inline ForwardIterator  __uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result, T *) {
+	using is_POD = __type_traits<T>::is_POD_type;
+	return __uninitialized_copy_aux(first, last, result, is_POD());
+}
+
+template<class InputIterator, class ForwardIterator, class T>
+inline ForwardIterator  __uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, __true_type) {
+	return copy(first, last, result);
+}
+
+template<class InputIterator, class ForwardIterator, class T>
+inline ForwardIterator  __uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, __false_type) {
+	ForwardIterator cur = result;
+	for (; first != last; ++cur,++first) {
+		construct(&*cur, *first);
+	}
+	return cur;
+}
+
+//针对char*、wchar_t*存在特化版本 memmove直接移动内存
+inline char*  uninitialized_copy(char* first, char* last, char* result) {
+	memmove(result, first, last - first);
+	return result + (last - first);
+}
+
+inline wchar_t*  uninitialized_copy(wchar_t* first, wchar_t* last, wchar_t* result) {
+	memmove(result, first, sizeof(wchar_t)*(last - first));
+	return result + (last - first);
+}
+
+template <class ForwardIterator, class T>
+inline ForwardIterator uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& value) {
+	return __uninitialized_fill(first, last, value, value_type(first));
+}
+
+template<class ForwardIterator, class T, class T1>
+inline ForwardIterator  __uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& value, T1*) {
+	using is_POD = __type_traits<T1>::is_POD_type;
+	return __uninitialized_fill_aux(first, last, value, is_POD());
+}
+
+template<class ForwardIterator, class T>
+inline ForwardIterator  __uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, const T& value, __true_type) {
+	return fill(first, last, value);
+}
+
+template<class ForwardIterator, class T>
+inline ForwardIterator  __uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, const T& value, __false_type) {
+	ForwardIterator cur = first;
+	for (:cur != last; ++cur) {
+		construct(&*cur, value);
+	}
+	return cur;
+}
+
+template<class ForwardIterator, class Size, class T>
+inline ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n, const T & x) {
+	//根据value_type取出first的value_type(详见迭代器一章）
+	return __uninitialized_fill_n(first, n, x, value_type(first));
+}
+
+template<class ForwardIterator, class Size, class T, class T1>
+inline ForwardIterator  __uninitialized_fill_n(ForwardIterator first, Size n, const T & x, T1*) {
+	//POD means Plain Old Data
+	//必然拥有trivial ctor/dtot/copy/assignment函数
+	using is_POD = __type_traits<T1>::is_POD_type;
+	return __uninitialized_fill_n_aux(first, n, x, is_POD());
+}
+
+template<class ForwardIterator, class Size, class T>
+inline ForwardIterator  __uninitialized_fill_n_aux(ForwardIterator first, Size n, const T & x,__true_type) {
+	//POD类型可直接采用初值填写式手法
+	return fill(first, n, x);//转交高阶函数执行
+}
+
+template<class ForwardIterator, class Size, class T>
+inline ForwardIterator  __uninitialized_fill_n_aux(ForwardIterator first, Size n, const T & x, __false_type) {
+	//忽略异常处理
+	//明确明确的是一旦一个对象构造失败则需要析构所有对象
+	ForwardIterator cur = first;
+	for (; n > 0; --n, ++cur) {
+		construct(&*cur, x);
+	}
+	return cur;
+}
+
