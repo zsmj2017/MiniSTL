14b9ca00ba893a4b108ceb0da816dcd2306a61c3
zsmj2017 <sphinxs@vip.qq.com>
2018-07-25 18:31:55 +0800

    Add stl_stack
    stack in C++ is not a container,but a adapter

---
+++ Sequence containers/Stack/stl_stack.h
@@ -0,0 +1,34 @@
+#pragma once
+
+//默认以deque实现
+//stack并非container，而是adapter
+template <class T,class Sequence = deque<T> >
+class stack {
+	//friend template
+	friend bool operator== <> (const stack&, const stack&);
+	friend bool operator< <> (const stack&, const stack&);
+public:
+	using value_type = Sequence::value_type;
+	using size_type = Sequence::size_type;
+	using reference = Sequence::reference;
+	using const_reference = Sequence::const_reference;
+protected:
+	Sequence c;//底层容器
+public:
+	bool empty() const { return c.empty(); }
+	size_type size() const { return c.size(); }
+	reference top() { return c.back(); }
+	const_reference top() const { return c.back(); }
+	void push(const value_type& value) { c.push_back(value); }
+	void pop() { c.pop_back(); }
+};
+
+template <class T, class Sequence>
+bool operator==(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
+	return lhs.c == rhs.c;
+}
+
+template<class T, class Sequence = deque<T> >
+inline bool operator<(const stack<T, Sequence> &, const stack<Sequence> &) {
+	return lhs.c < rhs.c;
+}
