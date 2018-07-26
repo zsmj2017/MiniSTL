0f42191d64132e563bb4ace4c85175c28a7c0c30
zsmj2017 <sphinxs@vip.qq.com>
2018-07-26 19:29:04 +0800

    Add rb_tree_node.h and rb_tree_iterator.h
    RB-Tree is the basic of map and set

---
+++ Associative containers/RB-Tree/rb_tree_iterator.h
@@ -0,0 +1,94 @@
+﻿#pragma once
+
+#include <cstddef>
+#include "rb_tree_node.h"
+
+struct __rb_tree_base_iterator {
+	using base_ptr = __rb_tree_node_base::base_ptr;
+	using iterator_category = bidirectional_iterator_tag;
+	using difference_type = ptrdiff_t;
+
+	base_ptr node;
+
+	void increment() {
+		if (node->right) {//存在右子，则下一节点必为右子树的最左下角
+			node = node->right;
+			while (node->left) node = node->left;
+		}
+		else {//当前不存在右子
+			base_ptr p = node->parent;
+			while (node == p->right) {//不断上溯，直至找到第一个不为右子的祖先
+				node = p;
+				p = p->parent;
+			}
+			//正常情况下该祖先之父即为结果
+			if (node->right != p) {
+				node = p;
+			}
+			//若右子等于父节点，node即为自增结果（此为特殊情况，适用于迭代器指向root而后者无right）
+		}
+	}
+
+	void decrement() {
+		if (node->color == __rb_tree_red && node->parent->parent == node)
+			//此为特例，当迭代器指向end()将触发此情况
+			//root存在一个父节点header，其颜色为红色，左右子均为root
+			node = node->left;
+		else if (node->left) {//存在左子，前一节点必然是左子树的最右下角
+			base_ptr l = node->left;
+			while (l->right){
+				l = l->right; 
+			}
+			node = l;
+		}
+		else {//既非root，亦无左子
+			base_ptr p = node->parent;
+			while (node==p->left){//不断上溯，直至找到第一个不为左子的祖先
+				node = p;
+				p = p->parent;
+			}
+			node = p;//该祖先的父亲即为答案
+		}
+	}
+};
+
+
+template <class T,class Ref,class Ptr>
+struct __rb_tree_iterator :public __rb_tree_base_iterator {
+	using value_type = T;
+	using reference = Ref;
+	using pointer = Ptr;
+	using iterator = __rb_tree_iterator<T, T&, T*>;
+	using const_iterator = __rb_tree_iterator<T, const T&, const T*>;
+	using self = __rb_tree_iterator<T, Ref, Ptr>;
+	using link_type = __rb_tree_node<T>*;
+
+	__rb_tree_iterator() {}
+	__rb_tree_iterator(link_type x) { node = x; }
+	__rb_tree_iterator(const iterator& it) { node = it.node; }
+
+	reference operator*() { return static_cast<link_type>(node)->value_field; }
+	pointer operator->() { return &(operator*()); }
+
+	self& operator++() {
+		increment();
+		return *this;
+	}
+
+	self operator++(int) {
+		self temp = *this;
+		increment();
+		return temp;
+	}
+
+	self& operator--() {
+		decrement();
+		return *this;
+	}
+
+	self operator--(int) {
+		self temp = *this;
+		decrement();
+		return temp;
+	}
+};
---
+++ Associative containers/RB-Tree/rb_tree_node.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+using __rb_tree_color_type = bool;
+const __rb_tree_color_type __rb_tree_red = false;
+const __rb_tree_color_type __rb_tree_black = true;
+
+struct __rb_tree_node_base {
+	using color_type = __rb_tree_color_type;
+	using base_ptr = __rb_tree_node_base* ;
+
+	color_type color;
+	base_ptr parent;
+	base_ptr left;
+	base_ptr right;
+
+	//本红黑树最小值
+	static base_ptr minium(base_ptr root) {
+		while (root->left) 
+			root = root->left;
+		return root;
+	}
+
+	//本红黑树最大值
+	static base_ptr maxium(base_ptr root) {
+		while (root->right) root = root->right;
+		return root;
+	}
+};
+
+template <class T>
+struct __rb_tree_node :public __rb_tree_node_base {
+	using link_type = __rb_tree_node<T>*;
+	T value_field;
+};
