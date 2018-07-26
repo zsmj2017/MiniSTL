372edcc5d60dd9ea5c9a4e498ca6790c30148a7c
zsmj2017 <sphinxs@vip.qq.com>
2018-07-26 12:42:47 +0800

    Add slist(not in STL)
    Add slist.h
    Add slist_iterator.h
    Add slist_node.h

---
+++ Sequence containers/Slist/slist.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+
+#include "slist_iterator.h"
+
+template <class T, class Alloc = alloc>
+class slist {
+public:
+	using value_type = T;
+	using pointer = T* ;
+	using const_pointer = const value_type*;
+	using reference = value_type& ;
+	using const_reference = const value_type&;
+	using size_type = size_t;
+	using difference_type = ptrdiff_t;
+
+	using iterator = __slist_iterator<T, T&, T*>;
+	using const_iterator = __slist_iterator<T, const T&, const T*>;
+
+private:
+	using list_node = __slist_node<T>;
+	using list_node_base = __slist_node_base;
+	using iterator_base = __slist_iterator_base;
+	using list_node_allocator = simple_alloc<list, node, Alloc>;
+
+	//配置与释放node
+	static list_node* create_node(const value_type& value) {
+		list_node* node = list_node_allocator::allocate();
+		try{
+			construct(&node->data, x);
+			node->next = 0;
+		}
+		catch{
+			list_node_allocator::deallocate(node);
+		}			
+		return node;
+	}
+
+	static void destroy_node(list_node* node) {
+		destroy(&node->data);
+		list_node_allocator::deallocate(node);
+	}
+
+private:
+	list_node_base head;//单链表头部,此并非指针，而是一个实体
+
+public:
+	slist() { head.next = nullptr; }
+	~slist() { clear(); }
+
+public:
+	iterator begin() { return iterator(static_cast<listnode*>(head.next); }
+	iterator end() { return iterator(nullptr); }
+	size_type size() const { return __slist_size(head.next); }
+	bool empty() const { return head.next == nullptr; }
+
+	void swap(slist& rhs) {
+		//交换单链表只需要交换彼此的第一个节点即可
+		list_node_base* temp = head.next;
+		head.next = rhs.head.next;
+		rhs.head.next = temp;
+	}
+
+public:
+	reference front() { return static_cast<list_node*>(head.next)->data; }
+
+	void push_front(const value_type& value) {
+		__slist_make_link(&head, create_node(value));
+	}
+
+	void pop_front() {
+		list_node* node = static_cast<list_node*>(head.next);
+		head.next = node->next;
+		destory_node(node);
+	}
+};
---
+++ Sequence containers/Slist/slist_iterator.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include <cstddef>
+#include "slist_node.h"
+
+struct __slist_iterator_base {
+	//Alias decalartion
+	using size_type = size_t;
+	using difference_type = ptrdiff_t;
+	using iterator_category = forward_iterator_tag;
+
+	//持有base_node
+	__slist_node_base* node;
+
+	//Constructor
+	__slist_iterator_base(__slist_node_base* n):node(n) {}
+
+	//前进
+	void incur() { node = node->next; }
+
+	//等值判断
+	bool operator ==(const __slist_iterator_base& rhs) {
+		return node == rhs.node;
+	}
+
+	bool operator !=(const __slist_iterator_base& rhs) {
+		return node != rhs.node;
+	}
+};
+
+template<class T, class Ref, class Ptr>
+struct __slist_iterator :public __slist_iterator_base {
+	using iterator = __slist_iterator<T, T&, T*>;
+	using const_iterator = __slist_iterator<T, const T&, const T*>;
+	using self = __slist_iterator<T, Ref, Ptr>;
+
+	using value_type = T;
+	using pointer = Ptr;
+	using reference = Ref;
+	using list_node = __slist_node<T>;
+
+	//Constructor
+	__slist_iterator(list_node* node) : __slist_iterator_base(node) {}//调用slit.end()时将触发__slist_iterator(nullptr)
+	__slist_iterator() : __slist_iterator_base(nullptr) {}
+	__slist_iterator(const iterator& rhs) : __slist_iterator_base(rhs.node) {}
+
+	//Dereference
+	reference opertaor*() { return static_cast<list_node*>(node)->data; }
+	pointer operator->() { return &(operator*()); }
+
+	//Incur
+	self& operator++() {
+		incur();
+		return *this;
+	}
+
+	self operator++(int) {
+		self temp;
+		incur();
+		return temp;
+	}
+};
