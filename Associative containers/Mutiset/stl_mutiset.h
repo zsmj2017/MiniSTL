3a33ed085e4f0b59085ab6ce199cfe6b0e42060d
zsmj2017 <sphinxs@vip.qq.com>
2018-07-28 12:27:08 +0800

    Add stl_mutiset.h

---
+++ Associative containers/Mutiset/stl_mutiset.h
@@ -0,0 +1,163 @@
+﻿#pragma once
+
+#include <functional>
+
+//mutiset基本类似于set，区别在于插入时使用insert_equal而非insert_equal
+
+//Forward declarations of operators == and <, needed for friend declarations.
+template <class Key, class Compare, class Alloc>
+class mutiset;
+
+template <class Key, class Compare, class Alloc>
+inline bool operator==(const mutiset<Key, Compare, Alloc>& lhs, const mutiset<Key, Compare, Alloc>& rhs);
+
+template <class Key, class Compare, class Alloc>
+inline bool operator<(const mutiset<Key, Compare, Alloc>& lhs, const mutiset<Key, Compare, Alloc>& rhs);
+
+template <class Key, class Compare = less<Key>, class Alloc = alloc>
+class mutiset {
+public:
+	//Set的key与value是同一个
+	using key_type = Key;
+	using value_type = Key;
+	using key_compare = Compare;
+	using value_compare = Compare;
+
+private:
+	template <class T>
+	struct identity :public unary_function<T, T> {//一元函数对象，看看后期能不能用function对象替换
+		const T& operator()(const T& x) const { return x; }
+	};
+
+	using rep_type = rb_tree < key_type, value_type, identity<value_type>, Alloc>;
+
+	rep_type t;//底层红黑树
+
+public:
+	using pointer = rep_type::const_pointer;
+	using const_pointer = rep_type::const_pointer;
+	using reference = rep_type::const_reference;
+	using const_reference = rep_type const_reference;
+	using iterator = rep_type::const_iterator;
+	using const_iterator = rep_type::const_iterator;
+	using reverse_iterator = rep_type::const_reverse_iterator;
+	using const_reverse_iterator = rep_type::const_reverse_iterator;
+
+	using size_type = rep_type::size_type;
+	using difference_type = rep_type::difference_type;
+	using allocator_type = rep_type::allocator_type;
+
+	//构造与拷贝
+
+	// set只能使用RB-tree的insert-unique(),不能使用insert-equal() 
+	mutiset():t(key_compare()) {}
+	explicit mutiset(const key_compare& comp) :t(comp) {}
+
+	template <class InputIterator>
+	mutiset(InputIterator first, InputIterator last)
+		: t(key_compare()) {
+		t.insert_equal(first, last);
+	}
+
+	template <class InputIterator>
+	mutiset(InputIterator first, InputIterator last, const key_compare& comp)
+		: t(comp) {
+		t.insert_equal(first, last);
+	}
+
+	mutiset(const mutiset<Key, Compare, Alloc>& x) : t(x.t) {}
+
+	mutiset<Key, Compare, Alloc>& operator=(const mutiset<Key, Compare, Alloc> x) {
+		t = x.t;//调用rb-tree的operator=
+		return *this;
+	}
+
+	//accessor
+
+	key_compare key_comp() const { return t.key_comp(); }
+	value_compare value_comp() const { return t.key_comp(); }//set的value_comp即为rb-tree的key_comp
+	allocator_type get_allocator() const { return t.get_allocator(); }
+
+	//基本迭代器所指位置
+	iterator begin() const { return _t.begin(); }
+	iterator end() const { return t.end(); }
+	reverse_iterator rbegin() const { return t.rbegin(); }
+	reverse_iterator rend() const { return t.rend(); }
+
+	//属性获取
+	bool empty() const { return t.empty(); }
+	size_type size() const { return t.size(); }
+	size_type max_size() const { return t.max_size(); }
+	void swap(mutiset<Key, Compare, Alloc>& x) { t.swap(x.t); }//此处调用的是rb-tree成员函数，而非stl swap
+
+
+	//insert && erase
+
+	std::pair<iterator, bool> insert(const value_type& x) {
+		pair<typename rep_type::iterator, bool> p = t.insert_equal(x);
+		return pair<iterator, bool>(p.first, p.second);
+	}
+
+	iterator insert(iterator pos, const value_type& x) {
+		using rep_iterator = rep_type::iterator;
+		return t.insert_equal(static_cast<rep_iterator&>(pos), x);
+	}
+	template <class InputIterator>
+	void insert(InputIterator first, InputIterator last) {
+		t.insert_equal(first, last);
+	}
+
+	void erase(iterator pos) {
+		using rep_iterator = rep_type::iterator;
+		t.erase(static_cast<rep_iterator&>(pos))
+	}
+
+	size_type erase(const key_type& x) {
+		return t.erase(x);
+	}
+
+	void erase(iterator first, iterator last) {
+		using rep_iterator = rep_type::iterator;
+		t.erase(static_cast<rep_iterator&>(first), static_cast<rep_iterator&>(last));
+	}
+
+	void clear() { t.clear(); }
+
+	//find
+
+	iterator find(const key_type& x) const { return t.find(x); }
+
+	size_type count(const key_type& x) const {
+		return t.find(x) == t.end() ? 0 : 1;
+	}
+
+	//返回不小于当前元素的第一个可插入的位置 
+	iterator lower_bound(const key_type& x) const {
+		return t.lower_bound(x);
+	}
+
+	// 返回大于当前元素的第一个可插入的位置 
+	iterator upper_bound(const key_type& x) const {
+		return t.upper_bound(x);
+	}
+
+	std::pair<iterator, iterator> equal_range(const key_type& x) const {
+		return t.equal_range(x);
+	}
+
+	//operator
+
+	friend bool operator== <>(const mutiset&, const mutiset&);
+	friend bool operator<  <>(const mutiset&, const mutiset&);
+};
+
+template <class Key, class Compare, class Alloc>
+inline bool operator==(const mutiset<Key, Compare, Alloc>& lhs, const mutiset<Key, Compare, Alloc>& rhs) {
+	return lhs.t == rhs.t;
+}
+
+
+template <class Key, class Compare, class Alloc>
+inline bool operator<(const mutiset<Key, Compare, Alloc>& lhs, const mutiset<Key, Compare, Alloc>& rhs) {
+	return lhs.t < rhs.t;
+}
