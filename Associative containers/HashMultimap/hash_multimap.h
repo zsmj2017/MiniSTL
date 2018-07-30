da0fcafd52a1269c65c6c5226254382f505cd580
zsmj2017 <sphinxs@vip.qq.com>
2018-07-30 12:56:57 +0800

    Fix template paramters bugs about hash_map.h and add hash_multimap.h

--- Associative containers/HashMultimap/hash_multimap.h
+++ Associative containers/HashMultimap/hash_multimap.h
@@ -1,15 +1,15 @@
 ﻿#pragma once
 
-template <class Key,class T, class HashFcn, class EqualKey class Alloc>
-class hash_map;
+template <class Key, class T, class HashFcn, class EqualKey class Alloc>
+class hash_multimap;
 
-template <class Key,class T, class HashFcn, class EqualKey, class Alloc>
-inline bool operator==(const hash_map<Key, T, HashFcn, EqualKey, Alloc>& lhs, const hash_map<Key, T, HashFcn, EqualKey, Alloc>& rhs);
+template <class Key, class T, class HashFcn, class EqualKey, class Alloc>
+inline bool operator==(const hash_multimap<Key, T, HashFcn, EqualKey, Alloc>& lhs, const hash_multimap<Key, T, HashFcn, EqualKey, Alloc>& rhs);
 
 template <class Key, class T, class HashFcn = hash<Value>, class EqualKey = equal_to<Value>, class Alloc = alloc >
-class hash_map{
+class hash_multimap {
 private:
-	using ht = hashtable<pair<const Key,T>, Key, HashFcn, select1st<pair<const Key,T>, EqualKey, Alloc>;
+	using ht = hashtable<pair<const Key, T>, Key, HashFcn, select1st<pair<const Key, T>, EqualKey, Alloc>;
 	ht rep;//底层数据结构
 
 public:
@@ -38,99 +38,97 @@ public:
 public:
 	//ctor
 	//默认构建一个size为100的hashtable
-	hash_map():rep(100, hasher(), key_equal(), allocator_type()) {}
-	explicit hash_map(size_type n) :rep(n, hasher(), key_equal(), allocator_type()) {}
-	hash_map(size_type n, const hasher& hf) :rep(n, hf, key_equal(), allocator_type()) {}
-	hash_map(size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
+	hash_multimap() :rep(100, hasher(), key_equal(), allocator_type()) {}
+	explicit hash_multimap(size_type n) :rep(n, hasher(), key_equal(), allocator_type()) {}
+	hash_multimap(size_type n, const hasher& hf) :rep(n, hf, key_equal(), allocator_type()) {}
+	hash_multimap(size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
 		:rep(n, hf, eql, a) {}
 
-	//以某范围的迭代器初构造hash_map，调用insert_unique
+	//以某范围的迭代器初构造hash_multimap，调用insert_equal
 	template <class InputIterator>
-	hash_map(InputIterator f, InputIterator l)
+	hash_multimap(InputIterator f, InputIterator l)
 		: rep(100, hasher(), key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 	template <class InputIterator>
-	hash_map(InputIterator f, InputIterator l, size_type n)
+	hash_multimap(InputIterator f, InputIterator l, size_type n)
 		: rep(n, hasher(), key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 	template <class InputIterator>
-	hash_map(InputIterator f, InputIterator l, size_type n, const hasher& hf)
+	hash_multimap(InputIterator f, InputIterator l, size_type n, const hasher& hf)
 		: rep(n, hf, key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 	template <class InputIterator>
-	hash_map(InputIterator f, InputIterator l, size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
+	hash_multimap(InputIterator f, InputIterator l, size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
 		: rep(n, hf, eql, a) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 
 	//C API
-	hash_map(const value_type* f, const value_type* l)
+	hash_multimap(const value_type* f, const value_type* l)
 		:rep(100, hasher(), key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
-	hash_map(const value_type* f, const value_type* l, size_type n)
+	hash_multimap(const value_type* f, const value_type* l, size_type n)
 		:rep(n, hasher(), key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
-	hash_map(const value_type* f, const value_type* l, size_type n, const hasher& hf)
+	hash_multimap(const value_type* f, const value_type* l, size_type n, const hasher& hf)
 		:rep(n, hf, key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
-	hash_map(const value_type* f, const value_type* l, size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
+	hash_multimap(const value_type* f, const value_type* l, size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
 		:rep(n, hf, eql, a) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 
-	hash_map(const_iterator f, const_iterator l)
+	hash_multimap(const_iterator f, const_iterator l)
 		:rep(100, hasher(), key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
-	hash_map(const_iterator f, const_iterator l, size_type n)
+	hash_multimap(const_iterator f, const_iterator l, size_type n)
 		:rep(n, hasher(), key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
-	hash_map(const_iterator f, const_iterator l, size_type n,
+	hash_multimap(const_iterator f, const_iterator l, size_type n,
 		const hasher& hf)
 		:rep(n, hf, key_equal(), allocator_type()) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
-	hash_map(const_iterator f, const_iterator l, size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
+	hash_multimap(const_iterator f, const_iterator l, size_type n, const hasher& hf, const key_equal& eql, const allocator_type& a = allocator_type())
 		:rep(n, hf, eql, a) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 
 public:
 	size_type size() const { return rep.size(); }
 	size_type max_size() const { return rep.max_size(); }
 	bool empty() const { return rep.empty(); }
-	void swap(hash_map& rhs) { rep.swap(rhs.rep); }
-	friend bool operator== <>(const hash_map&, const hash_map&);
-	iterator begin() const { return rep.begin(); }
-	iterator end() const { return rep.end(); }
+	void swap(hash_multimap& rhs) { rep.swap(rhs.rep); }
+	friend bool operator== <>(const hash_multimap&, const hash_multimap&);
+	iterator begin() { return rep.begin(); }
+	iterator end() { return rep.end(); }
+	const_iterator begin() const { return rep.begin(); }
+	const_iterator end() const { return rep.end(); }
 
 public:
-	pair<iterator, bool> insert(const value_type& obj) {
-		return rep.insert_unique(obj);
+	iterator insert(const value_type& obj) {
+		return rep.insert_equal(obj);
 	}
 	template <class InputIterator>
 	void insert(InputIterator f, InputIterator l) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 	void insert(const value_type* f, const value_type* l) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
 	void insert(const_iterator f, const_iterator l) {
-		rep.insert_unique(f, l);
+		rep.insert_equal(f, l);
 	}
-	pair<iterator, bool> insert_noresize(const value_type& obj) {
-		 return rep.insert_unique_noresize(obj);
-	}
-
-	data_type& operator[](const key_type& key) {
-		return rep.find_or_insert(value_type(key, data_type())).second;//见Effective STL Item24
+	iterator insert_noresize(const value_type& obj) {
+		return rep.insert_equal_noresize(obj);
 	}
 
 	size_type erase(const key_type& key) { return rep.erase(key); }
@@ -139,14 +137,14 @@ public:
 
 	void clear() { rep.clear(); }
 
-	iterator find(const key_type& key) const { return rep.find(key); }
+	iterator find(const key_type& key) { return rep.find(key); }
+	const_iterator find(const key_type& key) const { return rep.find(key); }
 
 	size_type count(const key_type& key) const { return rep.count(key); }
 
 	pair<iterator, iterator> equal_range(const key_type& key) {
 		return rep.equal_range(key);
 	}
-
 	pair<const_iterator, const_iterator> equal_range(const key_type& key) const {
 		return rep.equal_range(key);
 	}
@@ -161,6 +159,6 @@ public:
 };
 
 template <class Value, class HashFcn, class EqualKey, class Alloc>
-inline bool operator==(const hash_map<Key, T, HashFcn, EqualKey, Alloc>& lhs, const hash_map<Key, T, HashFcn, EqualKey, Alloc>& rhs) {
+inline bool operator==(const hash_multimap<Key, T, HashFcn, EqualKey, Alloc>& lhs, const hash_multimap<Key, T, HashFcn, EqualKey, Alloc>& rhs) {
 	return lhs.rep == rhs.rep;
 }
