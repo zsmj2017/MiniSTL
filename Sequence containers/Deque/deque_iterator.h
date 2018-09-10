3791ebd206bfdb01435826093894067c36d86e2d
zsmj2017 <sphinxs@vip.qq.com>
2018-09-10 17:59:13 +0800

    Fix some bugs and add its original contents to namespace:MiniSTL

--- Sequence containers/Deque/deque_iterator.h
+++ Sequence containers/Deque/deque_iterator.h
@@ -1,20 +1,24 @@
 ﻿#pragma once
 
 #include <cstddef>
+#include "stl_iterator.h"
+
+namespace MiniSTL {
 
 //缓冲区大小设定函数（在预设情况下传回可容纳的元素个数）
 //若n不为0，则传回n，表示由用户自定
 //若n为0则采用预设值 预设值根据sz（元素大小）而定
-inline size_t __deque_buf_size(size_t n, size_t sz){
-	return n ? n : (sz < 512 ? size_t(512 / sz) : size_t(1));
+inline size_t __deque_buf_size(size_t sz) {
+	return sz < 512 ? size_t(512 / sz) : size_t(1);
 }
 
-template<class T,class Ref,class Ptr,size_t BufSiz>
-struct __deque_iterator {//未继承stl_iterator
+template<class T, class Ref, class Ptr, size_t BufSiz>
+struct __deque_iterator {
+	// alias declarartions
 	using iterator = __deque_iterator<T, T&, T*, BufSiz>;
 	using const_iterator = __deque_iterator<T, const T&, const T*, BufSiz>;
 
-	static size_t buffer_size() { return __deque_buf_size(BufSiz, sizeof(T)); }
+	static size_t buffer_size() { return __deque_buf_size(sizeof(T)); }
 
 	using iterator_category = random_access_iterator_tag;
 	using value_type = T;
@@ -27,7 +31,7 @@ struct __deque_iterator {//未继承stl_iterator
 	using self = __deque_iterator;
 
 
-	//保持与容器的连接（我认为应当封装以下属性）
+	// 保持与容器的连接
 	T* cur;//当前缓冲区的当前元素
 	T* first;//当前缓冲区头
 	T* last;//当前缓冲区尾(含备用空间）
@@ -90,7 +94,7 @@ struct __deque_iterator {//未继承stl_iterator
 			difference_type node_offset = off_set > 0 ? off_set / static_cast<difference_type>(buffer_size())
 				: -static_cast<difference_type>((-off_set - 1) / buffer_size()) - 1;
 			node += node_offset;
-			cur = first + (off_set - static_cast<difference_type>(node_offset*(buffer_size()));
+			cur = first + (off_set - static_cast<difference_type>(node_offset*(buffer_size())));
 		}
 		return *this;
 	}
@@ -125,3 +129,4 @@ struct __deque_iterator {//未继承stl_iterator
 		return node == rhs.node ? cur < rhs.cur : node < rhs.node;
 	}
 };
+}//  end namespace::MiniSTL
