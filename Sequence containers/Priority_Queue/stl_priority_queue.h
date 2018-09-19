dcdae9ef65cc359cc11dd11b3771cf8cc649ac35
zsmj2017 <sphinxs@vip.qq.com>
2018-09-19 16:40:17 +0800

    Fix bugs and add original content to namespace::MiniSTL

--- Sequence containers/Priority_Queue/stl_priority_queue.h
+++ Sequence containers/Priority_Queue/stl_priority_queue.h
@@ -1,42 +1,48 @@
 ﻿#pragma once
+#include "stl_vector.h"
+#include "stl_function.h"
+#include "heap_algorithm.h"
 
-template <class T,class Sequence = vector<T>,class Compare = less<typename Sequence::value_type> >
+namespace MiniSTL {
+
+template <class T, class Sequence = vector<T>, class Compare = less<typename Sequence::value_type> >
 class priority_queue {
-public:
-	using value_type = Sequence::value_type;
-	using size_type = Sequence::size_type;
-	using reference = Sequence::reference;
-	using const_reference = Sequence::const_reference;
+public:// alias declarations
+	using value_type = typename Sequence::value_type;
+	using size_type = typename Sequence::size_type;
+	using reference = typename Sequence::reference;
+	using const_reference = typename Sequence::const_reference;
+	// no iterators
 
-protected:
+private:// data
 	Sequence c;
-	Compare comp;//比较器
+	Compare comp;
 
-public:
+public:// ctor && dtor
 	priority_queue() : c() {}
 	explicit priority_queue(const Compare& compare) : c(), comp(compare) {}
-	//此处make_heap为泛型算法，可接受比较器，在前一节撰写的heap算法中并不支持
+	priority_queue(std::initializer_list<T> il, const Compare& compare=Compare()):c(il),comp(compare) {
+		make_heap(c.begin(), c.end(), comp);
+	}
 	template <class InputIterator>
-	priority_queue(InputIterator first, InputIterator last, const Compare& compare)
+	priority_queue(InputIterator first, InputIterator last, const Compare& compare=Compare())
 		: c(first, last), comp(compare) {
 		make_heap(c.begin(), c.end(), comp);
 	}
-	template <class InputIterator>
-	priority_queue(InputIterator first, InputIterator last)
-		: c(first, last) {
-		make_heap(c.begin(), c.end(), comp);
-	}
+	~priority_queue() = default;
 
-public:
-	bool empty() const { return c.empty(); }
-	size_type size() const { return c.size(); }
-	const_reference top() const { return c.front(); }
+public: // getter
+	bool empty() const noexcept{ return c.empty(); }
+	size_type size() const noexcept{ return c.size(); }
+	const_reference top() const noexcept{ return c.front(); }
+
+public: // push && pop
 	void push(const value_type& value) {
-		try{
-			c.push_back(x);
+		try {
+			c.push_back(value);
 			push_heap(c.begin(), c.end(), comp);
 		}
-		catch{
+		catch(std::exception&){
 			c.clear();
 		}
 	}
@@ -45,8 +51,9 @@ public:
 			pop_heap(c.begin(), c.end(), comp);
 			c.pop_back();
 		}
-		catch{
+		catch (std::exception&) {
 			c.clear();
 		}
 	}
 };
+}
