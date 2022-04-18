53669ff1ab337c07e097def14b2ced052dcbe845
liuxiang <liuxiang@staff.weibo.com>
2022-04-18 11:40:10 +0800

    feat(generate_n()): add generate_n()

--- Test/Algorithms/algo/test_generator.cpp
+++ Test/Algorithms/algo/test_generator.cpp
@@ -10,8 +10,26 @@ class GeneratorTest : public testing::Test {
   }
 };
 
-// TODO::need generator such as rand() or fib()
-#if 0
+static int cxxrand() { return rand(); }
+
+class Fibonacci {
+ public:
+  Fibonacci() : v1(0), v2(1) {}
+  inline int operator()();
+
+ private:
+  int v1;
+  int v2;
+};
+
+inline int
+Fibonacci::operator()() {
+  int r = v1 + v2;
+  v1 = v2;
+  v2 = r;
+  return v1;
+}
+
 TEST_F(GeneratorTest, gener0) {
   int numbers[10];
   generate(numbers, numbers + 10, cxxrand);
@@ -35,7 +53,7 @@ TEST_F(GeneratorTest, gener1) {
 }
 TEST_F(GeneratorTest, genern1) {
   vector<int> v1(10);
-  generate_n(v1.begin(), v1.size(), ptr_gen(cxxrand));
+  generate_n(v1.begin(), v1.size(), cxxrand);
 }
 TEST_F(GeneratorTest, genern2) {
   vector<int> v1(10);
@@ -53,4 +71,3 @@ TEST_F(GeneratorTest, genern2) {
   ASSERT_TRUE(v1[8] == 34);
   ASSERT_TRUE(v1[9] == 55);
 }
-#endif
