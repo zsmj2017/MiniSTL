a5e3cc1307556ca20c8082e626a210d468996ddc
liuxiang <liuxiang@staff.weibo.com>
2022-07-21 15:32:51 +0800

    fix(type traits): fix bug about add_reference

--- Source/Utils/type_traits.h
+++ Source/Utils/type_traits.h
@@ -359,38 +359,45 @@ struct is_reference<T &> : true_type {};
 template<class T>
 struct is_reference<T &&> : true_type {};
 
+template<class T>
+struct type_identity {
+  using type = T;
+};
+
 // remove reference
 template<class T>
-struct remove_reference {
-  using type = T;
-};
+struct remove_reference : type_identity<T> {};
 
 template<class T>
-struct remove_reference<T &> {
-  using type = T;
-};
+struct remove_reference<T &> : type_identity<T> {};
 
 template<class T>
-struct remove_reference<T &&> {
-  using type = T;
-};
+struct remove_reference<T &&> : type_identity<T> {};
 
 template<class T>
 using remove_reference_t = typename remove_reference<T>::type;
 
+namespace detail {
 // add reference
 template<class T>
-struct add_lvalue_reference {
-  using type = T &;
-};
+auto try_add_lvalue_reference(int) -> type_identity<T &>;
+template<class T>
+auto try_add_lvalue_reference(...) -> type_identity<T>;
+
+template<class T>
+auto try_add_rvalue_reference(int) -> type_identity<T &&>;
+template<class T>
+auto try_add_rvalue_reference(...) -> type_identity<T>;
+}// namespace detail
+
+template<class T>
+struct add_lvalue_reference : decltype(detail::try_add_lvalue_reference<T>(0)) {};
+
+template<class T>
+struct add_rvalue_reference : decltype(detail::try_add_rvalue_reference<T>(0)) {};
 
 template<class T>
 using add_lvalue_reference_t = typename add_lvalue_reference<T>::type;
-
-template<class T>
-struct add_rvalue_reference {
-  using type = T &&;
-};
 
 template<class T>
 using add_rvalue_reference_t = typename add_rvalue_reference<T>::type;
